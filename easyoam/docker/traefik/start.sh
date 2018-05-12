4f9c67dafdc4542a46a87a0e5c454b780e39580f
Nazar Kulyk <kulyk@myeburg.net>
2018-05-12 16:56:02 +0200

    add traefik

--- easyoam/docker/traefik/start.sh
+++ easyoam/docker/traefik/start.sh
@@ -1,3 +1,3 @@
 !#/usr/bin/env bash
 
-docker run -d -p 8080:8080 -p 80:80 -v $PWD/traefik.toml:/etc/traefik/traefik.toml -v /var/run/docker.sock:/var/run/docker.sock traefik:1.6-apline
+docker run -d -p 8080:8080 -p 80:80 -p 443:443 -v $PWD/traefik.toml:/etc/traefik/traefik.toml -v /var/run/docker.sock:/var/run/docker.sock traefik:1.6-apline
