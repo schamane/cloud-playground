4c0fecf1c74f8e561cd24d179dc201fe6d5a836a
Nazar Kulyk <schamane@myeburg.net>
2018-05-13 03:13:05 +0200

    make executable

--- easyoam/docker/traefik/start.sh
+++ easyoam/docker/traefik/start.sh
@@ -1,3 +1,3 @@
 !#/usr/bin/env bash
 
-docker run -d -p 8080:8080 -p 80:80 -p 443:443 -v $PWD/traefik.toml:/etc/traefik/traefik.toml -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/acme.json:/etc/traefik/acme.json --restart always traefik:1.6-apline
+docker run -d -p 8080:8080 -p 80:80 -p 443:443 -v $PWD/traefik.toml:/etc/traefik/traefik.toml -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/acme.json:/etc/traefik/acme.json --restart always traefik:1.6-alpine
