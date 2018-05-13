fa16b1a9ff226d2af205f69e0ab1c2726a3731bd
Nazar Kulyk <schamane@myeburg.net>
2018-05-13 03:26:36 +0200

    fix conflict

--- easyoam/docker/traefik/start.sh
+++ easyoam/docker/traefik/start.sh
@@ -1,9 +1,5 @@
-!#/usr/bin/env bash
+#!/usr/bin/env bash
 
-<<<<<<< HEAD
-docker run -d -p 8080:8080 -p 80:80 -p 443:443 -v $PWD/traefik.toml:/etc/traefik/traefik.toml -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/acme.json:/etc/traefik/acme.json --restart always traefik:1.6-alpine
-=======
 docker run -d -p 8080:8080 -p 80:80 -p 443:443 \
 -v $PWD/traefik.toml:/etc/traefik/traefik.toml -v /var/run/docker.sock:/var/run/docker.sock \
 -v $PWD/acme.json:/etc/traefik/acme.json --restart always --name traefik traefik:1.6-alpine
->>>>>>> a374e2fcf81de416839bada3d348128345613874
