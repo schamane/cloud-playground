55296f9d663ba5ba3ec0c470569cf73837b9bba1
Nazar Kulyk <schamane@myeburg.net>
2018-07-28 18:30:05 +0200

    update traefik config

--- easyoam/docker/traefik/start.sh
+++ easyoam/docker/traefik/start.sh
@@ -5,7 +5,7 @@ docker rm traefik
 
 docker run -d -p 8080:8080 -p 80:80 -p 443:443 \
  -v "$PWD/traefik.toml:/etc/traefik/traefik.toml" -v "/var/run/docker.sock:/var/run/docker.sock" \
- -v "traefik_acme:/etc/traefik/acme" --restart always \
+ -v "$PWD/traefik_acme:/etc/traefik/acme" --restart always \
  --name traefik \
  traefik:1.6-alpine
 
